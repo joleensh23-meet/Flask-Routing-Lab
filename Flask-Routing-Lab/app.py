8c050fbd6e9efb4196bdcef602efe447856d9f56
joleensh23-meet <109904191+joleensh23-meet@users.noreply.github.com>
2022-07-26 12:18:56 +0300

    still working

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -1,18 +1,9 @@
-from flask import Flask, redirect, request, render_template, url_for
+from flask import Flask, render_template
+app = Flask(__name__)
+ 
+@app.route('/')
+def home():
+    return render_template('home.html')
 
-
-app = Flask(  # Create a flask app
-    __name__,
-    template_folder='templates',  # Name of html file folder
-    static_folder='static'  # Name of directory for static files
-)
-
-# Your code should be below
-
-
-
-
-# Your code should be above
-
-if __name__ == "__main__":  # Makes sure this is the main process
+if __name__=='__main__':
     app.run(debug=True)
